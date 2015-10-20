bcedb041e4dc9e85f3783eee6760d0fa64247d69
Chris Hooper <Chris-Hooper@users.noreply.github.com>
2015-10-20 14:50:54 -0400

    Added HomeView class

--- js/main.js
+++ js/main.js
@@ -1,12 +1,4 @@
 var app = {
-
-    findByName: function() {
-        console.log('findByName');
-        var self = this;
-		this.store.findByName($('.search-key').val(), function(employees) {
-			$('.employee-list').html(self.employeeLiTpl(employees));
-		});
-    },
 	
 	showAlert: function(message, title) {
 		if (navigator.notification) {
@@ -15,22 +7,13 @@ var app = {
 			alert(title ? (title + ": " + message) : message);
 		}
 	},
-	
-	renderHomeView: function() {
-		$('body').html(this.homeTpl());
-		$('.search-key').on('keyup', $.proxy(this.findByName, this));
-	},
 
     initialize: function() {
-        this.homeTpl = Handlebars.compile($("#home-tpl").html());
-		this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
-		
 		var self = this;
 		this.store = new LocalStorageStore(function() {
 			self.showAlert('Store Initialized', 'Info');
-			self.renderHomeView();
+			$('body').html(new HomeView(self.store).render().el);
 		});
-        $('.search-key').on('keyup', $.proxy(this.findByName, this));
     }
 
 };
